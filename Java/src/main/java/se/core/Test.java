a7fa563815d03f35fc7c0c9487642888ba0bc182
Alizadeh <126390072+Alizadeh-Makui@users.noreply.github.com>
2025-04-11 23:19:45 +0330

    Add files via upload

---
+++ Java/src/main/java/se/core/Test.java
@@ -0,0 +1,4 @@
+package se.core;
+
+public class Test {
+}
---
+++ Java/src/test/java/se/core/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
